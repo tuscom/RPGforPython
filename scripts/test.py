b6c750e9b9d31d30facba893e858975058910725
neko-can <34763150+neko-can@users.noreply.github.com>
2018-07-21 11:59:14 +0900

    first create

--- scripts/test.py
+++ scripts/test.py
@@ -10,11 +10,15 @@ class test:
         self.windowName = "test"
         self.screen = pygame.display.set_mode(self.windowSize)
 
+        self.Window = Window(self.screen)
+
     def Main(self):
         pygame.display.set_caption(self.windowName)
 
         while True:
             self.screen.fill(self.pageColor)
+
+            self.Window.Update()
 
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -22,5 +26,14 @@ class test:
                     sys.exit()
 
             pygame.display.update()
+
+class Window:
+    def __init__(self, screen):
+        self.screen = screen
+        self.player = pygame.image.load("../pictures/mon_016.bmp")
+        self.Update = self.TestWindow
+
+    def TestWindow(self):
+        self.screen.blit(self.player, (0, 0))
 
 test().Main()
